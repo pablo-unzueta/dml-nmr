14e3df31cd258bc519630e6405ef3bd068b95ccc
Pablo Unzueta <punzu001@ucr.edu>
2022-05-03 09:59:52 -0700

    recoded package for quality of life

---
+++ main.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import argparse
+import yaml
+from src import process
+from src import models
+
+def main():
+    parser = argparse.ArgumentParser(description="DML-NMR inputs")
+    parser.add_argument(
+        "--config_path",
+        default="config.yml",
+        type=str,
+        help="Location of config file (default: config.yml)",
+    )
+    parser.add_argument(
+        "--model_path",
+        default=None,
+        type=str,
+        help="path of the model files",
+    )
+    parser.add_argument(
+        "--data_path",
+        default=None,
+        type=str,
+        help="Location of data containing structures (json or any other valid format) and accompanying files",
+    )
+    parser.add_argument(
+        "--atom_type",
+        default=None,
+        type=str,
+        help="13C, 1H, 15N, or 17O",
+    )
+    parser.add_argument(
+        "--dft",
+        default=None,
+        type=str,
+        help="aev, lda, pbe, or pbe0",
+    )
+    parser.add_argument(
+        "--basis",
+        default=None,
+        type=str,
+        help="631g or sto3g",
+    )
+    parser.add_argument(
+        "--reprocess_aev",
+        default=None,
+        type=bool,
+        help="Generate AEV files. Can be time consuming if handling large amounts of data",
+    )
+    parser.add_argument(
+        "--reprocess_shieldings",
+        default=None,
+        type=bool,
+        help="Generate shieldings from gaussian .log files",
+    )
+
+    ##Get arguments from command line
+    args = parser.parse_args(sys.argv[1:])
+
+    ##Open provided config file
+    assert os.path.exists(args.config_path), (
+        "Config file not found in " + args.config_path
+    )
+    with open(args.config_path, "r") as ymlfile:
+        config = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+    if args.data_path != None:
+        config["Processing"]["data_path"] = args.data_path
+    if args.atom_type != None:
+        config["Processing"]["atom_type"] = args.atom_type
+    if args.dft != None:
+        config["Processing"]["dft"] = args.atom_type
+    if args.basis != None:
+        config["Processing"]["basis"] = args.atom_type
+    if args.reprocess_aev != None:
+        config["Processing"]["reprocess_aev"] = args.reprocess_aev
+    if args.reprocess_shieldings != None:
+        config["Processing"]["reprocess_shieldings"] = args.shieldings
+
+    if config["Processing"]["dft"] == "aev":
+        model_path = os.path.join("src/saved_nets", config["Processing"]["dft"])
+    else:
+        model_path = os.path.join("src/saved_nets",
+                              config["Processing"]["dft"] + "_" +
+                              config["Processing"]["basis"])
+
+    config["Processing"]["model_path"] = model_path
+
+    print("------------------------------------------------")
+    print(f'Processing data from {config["Processing"]["data_path"]}')
+    print("------------------------------------------------")
+
+    process.process_data(config)
+
+    print("------------------------------------------------")
+    print(f'Loading model weights from {config["Processing"]["model_path"]}')
+    print("------------------------------------------------")
+
+    model = models.ensemble_net(config)
+    model.calc_dml_nmr(config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/process/process.py
@@ -0,0 +1,74 @@
+import re
+import os
+import torch
+import torchani
+import numpy as np
+from ase.io import read, write
+import string
+
+
+def process_data(config):
+    if config["Processing"]["reprocess_shieldings"]:
+        get_shieldings_from_log(config)
+    if config["Processing"]["reprocess_aev"]:
+        gen_aev(config)
+
+
+def get_shieldings_from_log(config):
+
+    # nmr_dict = {"13C": "C", "1H": "H", "15N": "N", "17O": "O"}
+    # atom_type = nmr_dict[config["Processing"]["atom_type"]]
+    # search_term = atom_type + "    Isotropic"
+    print('Generating Shift files...')
+    search_term = "    Isotropic"
+    pattern = re.compile(search_term)
+
+    for filename in sorted(os.listdir(config["Processing"]["data_path"])):
+        if filename.endswith('.log'):
+            shift_file = os.path.join(config["Processing"]["data_path"], filename[:-4] + ".shifts")
+
+            path = os.path.join(config["Processing"]["data_path"], filename)
+            with open(path, 'r') as f:
+                with open(shift_file, 'w') as sf:
+                    for line in f:
+                        match = re.search(pattern, line)
+                        if match:
+                            shieldings = line.split()
+                            sf.write(shieldings[4])
+                            sf.write('\n')
+
+def gen_aev(config):
+    device = torch.device('cpu')
+
+    model = torchani.models.ANI1ccx()
+    path = config["Processing"]["data_path"]
+    print('Generating AEV files...')
+
+    for filename in sorted(os.listdir(path)):
+        if filename.endswith('.log'):
+            file = os.path.join(path, filename)
+            # print(file)
+
+            # Read in molecule using ASE
+            molecule = read(file)
+
+            # Atom codes for ANI code
+            atoms = model.species_to_tensor(molecule.symbols).to(device).unsqueeze(0)
+            coords = torch.tensor([molecule.get_positions()], requires_grad=False, device=device)
+
+            # Generate AEV
+            _, aev = model.aev_computer((atoms, coords))
+
+            # HCNO atoms
+            atoms = molecule.get_atomic_numbers()
+            atoms.astype(int)
+
+            final_aev = np.column_stack((molecule.get_atomic_numbers(), aev.data.cpu().numpy()[0]))
+
+            # Save AEV
+            aev_file = filename[:-3] + "aev"
+            aev_file_path = os.path.join(path, aev_file)
+            # print(final_aev)
+            # np.savetxt(aev_file_path, aev.data.cpu().numpy()[0])
+            np.savetxt(aev_file_path, final_aev, fmt=','.join(['%i'] + ['%1.8E'] * 384))
+            # np.savetxt(aev_file_path, final_aev, fmt=' '.join(['%i'] + ['%1.8E']*384))
